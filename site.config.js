4b7179228d155c9f47944f6836722c938bdf75d6
jinchen <jinchenhu.luke@gmail.com>
2021-06-09 16:40:47 -0400

    updated

--- site.config.js
+++ site.config.js
@@ -9,7 +9,7 @@ module.exports = {
   // basic site info (required)
   name: 'JinchenHu',
   domain: 'jinchenhu.it',
-  author: 'Travis Fischer',
+  author: 'Jinchen Hu',
 
   // open graph metadata (optional)
   description: 'Example site description',
